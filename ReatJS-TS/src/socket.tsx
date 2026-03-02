d8831f8527bd6ef16fd10b1d842f55024bb16ecc
Viết Tài <trantafi204@gmail.com>
2026-03-03 02:37:38 +0700

    fix(socket): add withCredentials to socket.io client for cross-origin authentication

--- ReatJS-TS/src/socket.tsx
+++ ReatJS-TS/src/socket.tsx
@@ -1,4 +1,6 @@
 import { io } from "socket.io-client";
-const socket = io(import.meta.env.VITE_SOCKET_URL);
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  withCredentials: true,
+});
 
 export default socket;
